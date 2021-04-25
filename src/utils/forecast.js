b9595d73ea9a8cd8c5a14d4e6f8aa5e89cce82b0
chunkymonkey0 <hussein.chebli@lau.edu>
2021-04-26 00:23:31 +0300

    Added more forecast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (longitude, latitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=2ebd1a9c1802e4b2fd2abc5c019dec79&query=' + latitude + ',' + longitude + '&units=f'
   request({ url, json: true }, (error, { body }) => {
-    
+
     if (error) {
       callback("Unable to connect to weather services!", undefined)
     } else if (body.error) {
       callback("Unable to find location", undefined)
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out."
+      callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The Humidity is " + body.current.humidity + "%."
       )
     }
   })
